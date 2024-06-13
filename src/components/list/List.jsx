684fbd5889471e5bbc2c53c5c5c25a559010a076
Vladislav Lebedkin <vladislav_lebedkin@mail.ru>
2024-06-13 21:08:27 +0300

    подключил компоненты и стилизовал их

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,7 +1,15 @@
-import "./list.css";
+import './list.css';
+
+import ChatList from './chatList/ChatList';
+import UserInfo from './userInfo/UserInfo';
 
 const List = () => {
-	return <div className="list">List</div>;
+	return (
+		<div className='list'>
+			<UserInfo />
+			<ChatList />
+		</div>
+	);
 };
 
 export default List;
