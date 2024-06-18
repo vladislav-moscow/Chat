0ec1ed01986af7edb86f5263476bb188e0f5c51b
Vladislav Lebedkin <vladislav_lebedkin@mail.ru>
2024-06-18 20:01:42 +0300

    в компонент список чатов добавил поиск

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,7 +1,28 @@
-import "./chatList.css";
+'use client';
+import Image from 'next/image';
+import './chatList.css';
+import { useState } from 'react';
 
 const ChatList = () => {
-	return <div className="chatList">ChatList</div>;
+	const [addMode, setAddMode] = useState(false);
+	return (
+		<div className='chatList'>
+			<div className='search'>
+				<div className='searchBar'>
+					<Image src='/search.png' width={20} height={20} alt='поиск' />
+					<input type='text' placeholder='Поиск...' />
+				</div>
+				<Image
+					src={addMode ? '/minus.png' : '/plus.png'}
+					width={36}
+					height={36}
+					alt='добавить'
+					className='add'
+					onClick={() => setAddMode((prev) => !prev)}
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default ChatList;
